3fc0bc2e6594501437ff15e329f8fdf4a57942a5
TK Tran <tktrannn@live.com>
2022-03-18 17:22:10 -0400

    Simplified HTML and CSS.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 // Defines Global Routes
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AppComponent },
+];
 
 @NgModule({
   declarations: [],
